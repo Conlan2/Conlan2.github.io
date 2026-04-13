635943c889e2127b93e4bb7913070293de6dd4ab
Conlan <Conlan@DESKTOP-7893V0U>
2026-04-13 10:56:36 -0600

    Updated for github pages

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/conlan2.github.io",
 };
 
 export default nextConfig;
