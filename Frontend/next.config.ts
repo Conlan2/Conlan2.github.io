51fb3fd61156d74ce5087093423ed6703f1510f4
Conlan2 <63008153+Conlan2@users.noreply.github.com>
2026-04-13 11:51:06 -0600

    Update asset Prefix for images

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   basePath: "/conlan2.github.io",
+  assetPrefix: "/conlan2.github.io",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
